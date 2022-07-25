57f99bb29668cad9bcc8ffd4ba37a2b7c5941b19
Liza Denisova <golliza@yandex.ru>
2022-07-26 01:39:26 +0300

    attempt 4

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,23 +1,24 @@
 """Basic functions for Calc-game"""
 
-import operator
 import random
-from random import choice
 RULES = 'What is the result of the expression?'
 LOWER_BOUND = 1
 UPPER_BOUND = 15
 
 
+def get_operator(num1, num2, operator):
+    if operator == '+':
+        return str(num1 + num2)
+    elif operator == '-':
+        return str(num1 - num2)
+    else:
+        return str(num1 * num2)
+
+
 def get_question_answer():
     num1 = random.randint(LOWER_BOUND, UPPER_BOUND)
     num2 = random.randint(LOWER_BOUND, UPPER_BOUND)
-    operators_list = {'+': operator.add, '-': operator.sub, '*': operator.mul}
-    operation = choice(list(operators_list.keys()))
-    if operation == "+":
-        correct_answer = num1 + num2
-    elif operation == "-":
-        correct_answer = num1 - num2
-    elif operation == "*":
-        correct_answer = num1 * num2
-    question = f'{num1} {operation} {num2}'
-    return question, str(correct_answer)
+    operators = random.choice(['+', '-', '*'])
+    question = f'{num1} {operators} {num2}'
+    correct_answer = get_operator(num1, num2, operators)
+    return question, correct_answer
